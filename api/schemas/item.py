873f1a484853b0f6b967e4e7299e62dfd7757d6c
Cameron Abeel <camabeel@Camerons-MacBook-Pro.local>
2024-11-25 19:17:48 -0500

    database setup

--- api/schemas/item.py
+++ api/schemas/item.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field
 
 class Item(BaseModel):
+    __tablename__ = "items"
+    
     shortDescription: str = Field(description="The Short Product Description for the item.", pattern="^[\\w\\s\\-]+$", examples=["Mountain Dew 12PK"])
     price: float = Field(description="The total price payed for this item.", example=6.49)
--- api/schemas/receipt.py
+++ api/schemas/receipt.py
@@ -3,6 +3,8 @@ from datetime import date, time
 from api.schemas.item import Item
 
 class Receipt(BaseModel):
+    __tablename__ = "receipts"
+    
     retailer: str = Field(description="The name of the retailer or store the receipt is from.", pattern="^[\\w\\s\\-&]+$", examples=["M&M Corner Market"])
     purchaseDate: date = Field(description="The date the receipt was issued.", example="2021-01-01")
     purchaseTime: time = Field(description="The time the receipt was issued.", example="13:01", pattern="^([01]?[0-9]|2[0-3]):[0-5][0-9]$")
